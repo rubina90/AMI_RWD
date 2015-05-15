ca67bf975d62e9176c6b41c99d10dee2fdc7faeb
Waldemar Bira <waldek.bira@gmail.com>
2015-05-15 10:13:49 +0200

    scroll fix

--- Scripts/scroll.js
+++ Scripts/scroll.js
@@ -1,9 +1,4 @@
-function scrollToAnchor(aid){
-    var aTag = $(aid);
-    $('html,body').animate({scrollTop: aTag.offset().top - 75},'slow');
-}
-
-$(".scroll-link").click(function() {
- var href = $(this).attr('href');
- scrollToAnchor(href);
+$(".scroll-link").click(function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
 });
