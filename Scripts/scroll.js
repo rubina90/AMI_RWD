5f121308d15dd6db66676b08f5e2e52c5fbd37e6
Waldemar Bira <waldek.bira@gmail.com>
2015-05-15 11:35:47 +0200

    top-slider - fix

--- Scripts/scroll.js
+++ Scripts/scroll.js
@@ -1,4 +1,4 @@
 $(".scroll-link").click(function(event){
     event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
+    $('html,body').animate({scrollTop:$(this.hash).offset().top - 75}, 1500);
 });
